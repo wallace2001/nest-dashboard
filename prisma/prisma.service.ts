0169a85a21d44559ecc5688e4b67909d12e5660d
Wallace <dev_kollen@outlook.com>
2024-08-14 18:19:35 -0300

    fix

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  // Opcionalmente, você pode usar beforeApplicationShutdown para garantir o fechamento
+  async beforeApplicationShutdown() {
+    await this.$disconnect();
   }
 }
