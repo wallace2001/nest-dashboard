4998ea11ee5434cb936fe8b800e8d6771f146501
wallace <wallace.silva@nscreen.com.br>
2025-02-24 13:22:58 -0300

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as express from 'express';
 import { UsersModule } from './user.module';
 import cookieParser = require("cookie-parser");
 
-const expressApp = express();
-
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(UsersModule, new ExpressAdapter(expressApp));
+  const app = await NestFactory.create<NestExpressApplication>(UsersModule);
 
   app.enableCors({
     origin: '*'
